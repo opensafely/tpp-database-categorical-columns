5249fbf279c6d171c9567329e74bd1ae942abcc7
Iain Dillingham <iain@dillingham.me.uk>
2023-11-20 14:35:56 +0000

    `query_practice_registrations` -> `query_tpp`

--- analysis/query_tpp.sql
+++ analysis/query_tpp.sql
@@ -2,18 +2,21 @@
 -- Consequently, this query doesn't reference the PatientsWithTypeOneDissent table.
 WITH categorical_columns AS (
     SELECT DISTINCT
+        'practice_registrations' AS table_name,
         'practice_stp' AS column_name,
         STPCode AS column_value
     FROM Organisation
     UNION ALL
     SELECT DISTINCT
+        'practice_registrations' AS table_name,
         'practice_nuts1_region_name' AS column_name,
         Region AS column_value
     FROM Organisation
 )
 
 SELECT
+    table_name,
     column_name,
     column_value
 FROM categorical_columns
-ORDER BY column_name, column_value
+ORDER BY table_name, column_name, column_value
