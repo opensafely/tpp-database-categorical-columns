a9a671d8cac27736c2a0233343565718a7bde681
Iain Dillingham <iain@dillingham.me.uk>
2023-11-21 16:13:43 +0000

    Separate `patients` and `ons_deaths`

--- analysis/query_core.sql
+++ analysis/query_core.sql
@@ -1,12 +1,15 @@
 -- This query is for a data development project that doesn't include T1OO data.
 -- Consequently, this query doesn't reference the PatientsWithTypeOneDissent table.
 WITH categorical_columns AS (
+    -- patients
     SELECT DISTINCT
         'patients' AS table_name,
         'sex' AS column_name,
         Sex AS column_value
     FROM Patient
     UNION ALL
+
+    -- ons_deaths
     SELECT DISTINCT
         'ons_deaths' AS table_name,
         'place' AS column_name,
